dd2ae12a8247c5e29c91e112d9e17bd5204900e1
Akash Kushwaha <kushwahaakash746@gmail.com>
2024-08-15 22:14:27 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('user_role_type_id')->comment('1=>Teacher, 2=>Student');
             $table->tinyInteger('user_active')->default((1));
