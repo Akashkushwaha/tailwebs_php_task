958a5649511f2d53cfb2cd89922fa6794d233dfb
Akash Kushwaha <kushwahaakash746@gmail.com>
2024-08-15 22:30:21 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,18 @@ Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-Route::get('register',[LoginController::class, 'register'])->name('register');
-Route::post('register',[LoginController::class, 'register_user'])->name('register.post');
+Route::middleware(['guest'])->group(function () {
+    Route::get('register', [LoginController::class, 'register'])->name('register');
+    Route::post('register', [LoginController::class, 'register_user'])->name('register.post');
+    
+    Route::get('login', [LoginController::class, 'login'])->name('login');
+    Route::post('login', [LoginController::class, 'attempt_login'])->name('login.post');
+});
 
-Route::get('login',[LoginController::class, 'login'])->name('login');
-Route::post('login',[LoginController::class, 'attempt_login'])->name('login.post');
-Route::post('logout',[LoginController::class, 'logout'])->name('logout');
-
-Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
-
-Route::get('get-student-records',[DashboardController::class,'get_student_records'])->name('get.student.records');
-Route::post('edit-record',[DashboardController::class,'edit_record'])->name('edit.record');
-Route::post('delete-record',[DashboardController::class,'delete_record'])->name('delete.student');
+Route::middleware(['auth'])->group(function () {
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('get-student-records', [DashboardController::class, 'get_student_records'])->name('get.student.records');
+    Route::post('edit-record', [DashboardController::class, 'edit_record'])->name('edit.record');
+    Route::post('delete-record', [DashboardController::class, 'delete_record'])->name('delete.student');
+});
